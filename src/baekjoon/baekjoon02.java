2c86cd14c51014792e9e31214c2832a116ec6aa0
user <user@DESKTOP-G6EVCFO>
2021-10-22 10:54:38 +0900

    baekjoon

---
+++ src/baekjoon/baekjoon02.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+public class baekjoon02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("baekjoon solution");
+
+	}
+
+}
